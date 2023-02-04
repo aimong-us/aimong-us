a6adb0adb074b0ed43a24e6143d67d25a1c5902b
Julien Devlin <juliendevlin@gmail.com>
2023-02-04 10:43:42 -0500

    fill out index.js and add app and chat components

---
+++ client/app.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import Chat from './components/chat.jsx';
+
+const App = () => {
+  return <Chat />;
+};
+
+export default App;
---
+++ client/components/chat.jsx
@@ -0,0 +1,5 @@
+const Chat = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default Chat;
